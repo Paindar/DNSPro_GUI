32eea52fad164efd8ccaccd44b9d630771a36f57
Paindar <16387007+Paindar@users.noreply.github.com>
2019-07-09 21:48:13 +0800

    Fix: Udp invoke crash.

--- Network/Stages/UdpConnForwardRecv.cs
+++ Network/Stages/UdpConnForwardRecv.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace DNSPro_GUI.Network.Stages
 {
@@ -13,6 +14,7 @@ namespace DNSPro_GUI.Network.Stages
         private IPEndPoint endPoint;
         private byte[] header;
         private UdpClient forwardClient = new UdpClient();
+        private Timer timer;
 
         public UdpConnForwardRecv(UdpConn conn, IPEndPoint endPoint, byte[] header) : base(conn)
         {
@@ -32,16 +34,46 @@ namespace DNSPro_GUI.Network.Stages
 
         private void onSendEnd(IAsyncResult ar)
         {
-            forwardClient.EndSend(ar);
+            try
+            { 
+                forwardClient.EndSend(ar);
+            }
+            catch(ObjectDisposedException)
+            {
+                ;
+            }
+            catch(SocketException e)
+            {
+                return;
+            }
             forwardClient.BeginReceive(new AsyncCallback(onRecvEnd), null);
+            timer = new Timer(6000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
         }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         private void onRecvEnd(IAsyncResult ar)
         {
-            IPEndPoint e1 = new IPEndPoint(0, 0);
-            byte[] buff = forwardClient.EndReceive(ar, ref e1);
-            Logging.Info("End request id " + header[0] + header[1]);
-            conn.SendBack(buff);
-            conn.Close();
+            try
+            {
+                IPEndPoint e1 = new IPEndPoint(0, 0);
+                byte[] buff = forwardClient.EndReceive(ar, ref e1);
+                Logging.Info("End request id " + header[0] + header[1]);
+                timer.Elapsed -= Timer_Elapsed;
+                timer.Stop();
+                conn.SendBack(buff);
+                conn.Close();
+            }
+            catch(ObjectDisposedException)
+            {
+                ;
+            }
+            
         }
     }
 }
